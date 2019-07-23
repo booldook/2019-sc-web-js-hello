99d17b30c62a6143931d8d3cf53dd7e8296d0b21
booldook <booldook@gmail.com>
2019-07-23 15:42:26 +0900

    fa-bars

--- js/04.ani-exam.js
+++ js/04.ani-exam.js
@@ -1,9 +1,24 @@
-$(".navi-wrap").mouseenter(function(e){
-	e.stopPropagation();
-	$(this).children(".navi-bg").stop().animate({"width": "90px", "height": "90px"}, 300);
+$(".fa-bars").mouseenter(function(){
+	$(this).css({"color": "#333"});
+	$(this).prev().stop().animate({"width": "90px", "height": "90px"}, 300, function(){
+		$(this).stop().animate({"height": "100%"}, 150, function() {
+			$(this).stop().animate({"width": "100%"}, 300, function(){
+				$(".fa-bars").off("mouseleave");
+				$(".fa-bars").hide();
+				$(".fa-times").show();
+			});
+		});
+	});
 });
 
-$(".navi-wrap").mouseleave(function(e){
-	e.stopPropagation();
-	$(this).children(".navi-bg").stop().animate({"width": 0, "height": 0}, 300);
-});
+$(".fa-bars").on("mouseleave", leaveFn);
+$(".fa-times").on("click", leaveFn);
+
+function leaveFn(){
+	$(".fa-bars").css({"color": "#f9f9f9"});
+	$(".navi-bg").stop().animate({"width": 0, "height": 0}, 200, function(){
+		$(".fa-bars").show();
+		$(".fa-times").hide();
+		$(".fa-bars").on("mouseleave", leaveFn);
+	});
+}

f8a1e00a23782c27abb56be6fab7e8b4bb9423c5
booldook <booldook@gmail.com>
2019-07-23 13:44:23 +0900

    02.animate

--- js/03.ani.js
+++ js/03.ani.js
@@ -85,3 +85,36 @@ $(".vtc-nav").hover(function(){
 	$(this).removeClass("bg-secondary text-light");
 	$(this).children("ul").stop().slideUp(300);
 });
+
+
+// animate()
+/* 
+animate({value}, speed, ease, function(){});
+- {value}				: https://www.w3schools.com/jquery/eff_animate.asp
+									backgroundPositionX, backgroundPositionY
+									borderWidth, borderBottomWidth, borderLeftWidth
+									borderRightWidth, borderTopWidth, borderSpacing
+									margin, marginBottom, marginLeft, marginRight, marginTop
+									opacity
+									outlineWidth
+									padding, paddingBottom, paddingLeft, paddingRight, paddingTop
+									height, width, maxHeight, maxWidth, minHeight, minWidth
+									fontSize
+									bottom, left, right, top
+									letterSpacing, wordSpacing, lineHeight, textIndent
+- speed					: (ms:밀리초 - 1/1000초) 1000: 1초, 2000: 2초
+- ease 					: swing(css: ease-in-out) | linear(css: linear정속) 
+- function(){}	: 콜백(선언해 놓으면 애니메이션이 완료된 후 실행된다.)
+*/
+$("#bt-ani1").click(function(){
+	$("#box-ani1").stop().animate({
+		"opacity": 0,
+		"height": "500px"
+	}, 2000, function(){
+		$(this).stop().animate({
+			"opacity": 1,
+			"height": "200px"
+		}, 300);
+		//alert("Animation이 완료되었습니다.");
+	});
+});

c640896dc5457ff053a617e0cc9c1f6d9a57f487
booldook <booldook@gmail.com>
2019-07-23 15:00:27 +0900

    fa-bars

--- js/04.ani-exam.js
+++ js/04.ani-exam.js
@@ -1,7 +1,9 @@
-$(".fa-bars").mouseenter(function(){
-	
+$(".navi-wrap").mouseenter(function(e){
+	e.stopPropagation();
+	$(this).children(".navi-bg").stop().animate({"width": "90px", "height": "90px"}, 300);
 });
 
-$(".fa-bars").mouseleave(function(){
-	
+$(".navi-wrap").mouseleave(function(e){
+	e.stopPropagation();
+	$(this).children(".navi-bg").stop().animate({"width": 0, "height": 0}, 300);
 });

ba576afb79892e3c651897f110fbbcfcc48892f5
booldook <booldook@gmail.com>
2019-07-29 10:06:50 +0900

    11.slide

---
+++ js/11.slide.js
@@ -0,0 +1,20 @@
+/*
+setInterval(함수, 지연시간)	=> 지연시간마다 함수를 실행한다
+var i = 0;
+var interval = setInterval(function(){
+	console.log(i++);
+}, 1000);
+clearInterval(interval);	// setInterval 삭제하기
+*/
+/* <li class="list-group-item">1</li> */
+$("#btStart").click(function(){
+	
+});
+
+$("#btEnd").click(function(){
+
+});
+
+$("#btReset").click(function(){
+	$("#stage").empty();
+});

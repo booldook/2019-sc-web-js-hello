80aab44cd85cb5e0ff624481f0df1f4a680d89af
booldook <booldook@gmail.com>
2019-07-29 16:17:12 +0900

    12.slide2 obj 변경

--- js/12.slide2.js
+++ js/12.slide2.js
@@ -10,25 +10,24 @@ var slides = [
 
 // 전역변수
 var interval;
-var gap = 3000;
-var speed = 500;
-var now = 1;
-var $slide = [];
+var gap = 3000;			// 3초에 한번씩 애니메이션 실행
+var speed = 500;		// 애니메이션 속도가 0.5s
+var now = 1;				// 현재 0번에서 다음 1번으로 이동시키려 함.
+var obj;
 
 // 초기화
 for(var i in slides) {
-	$slide[i] = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');
-	$slide[i].css({"left": (i*100)+"%"});
+	obj = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');
+	obj.css({"left": (i*100)+"%"});
 	if(i == 0) $(".pager").append('<li class="cir-sel"></li>');
 	else if(i < slides.length-1) $(".pager").append('<li class="cir"></li>');
 }
-
 
 // 반복, 움직임
 interval = setInterval(function(){
 	$(".slides").stop().animate({"left": -(now*100)+"%"}, speed, function(){
 		$(".pager > li").removeClass("cir-sel").addClass("cir");
-		if(now == $slide.length - 1) {
+		if(now == slides.length - 1) {
 			$(this).css({"left": 0});
 			$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
 			now = 1;

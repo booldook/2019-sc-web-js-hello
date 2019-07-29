9266f342de5626115e5870cf120e2d91ae0872d6
booldook <booldook@gmail.com>
2019-07-29 15:34:00 +0900

    12.slide2

--- js/12.slide2.js
+++ js/12.slide2.js
@@ -19,15 +19,23 @@ var $slide = [];
 for(var i in slides) {
 	$slide[i] = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');
 	$slide[i].css({"left": (i*100)+"%"});
+	if(i == 0) $(".pager").append('<li class="cir-sel"></li>');
+	else if(i < slides.length-1) $(".pager").append('<li class="cir"></li>');
 }
+
 
 // 반복, 움직임
 interval = setInterval(function(){
 	$(".slides").stop().animate({"left": -(now*100)+"%"}, speed, function(){
+		$(".pager > li").removeClass("cir-sel").addClass("cir");
 		if(now == $slide.length - 1) {
 			$(this).css({"left": 0});
+			$(".pager > li").eq(0).removeClass("cir").addClass("cir-sel");
 			now = 1;
 		}
-		else now++;
+		else {
+			$(".pager > li").eq(now).removeClass("cir").addClass("cir-sel");
+			now++;
+		}
 	});
 }, gap);

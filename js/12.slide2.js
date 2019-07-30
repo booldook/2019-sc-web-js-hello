042a91e6e6e13ff7a31be3f6da5c792fcd74fe64
booldook <booldook@gmail.com>
2019-07-30 09:48:22 +0900

    each()

--- js/12.slide2.js
+++ js/12.slide2.js
@@ -13,15 +13,37 @@ var interval;
 var gap = 3000;			// 3초에 한번씩 애니메이션 실행
 var speed = 500;		// 애니메이션 속도가 0.5s
 var now = 1;				// 현재 0번에서 다음 1번으로 이동시키려 함.
-var obj;
+// var obj;
+var html;
 
 // 초기화
 for(var i in slides) {
-	obj = $('<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>').appendTo('.slides');
-	obj.css({"left": (i*100)+"%"});
+	html = '<div class="slide"><img src="'+slides[i].src+'" class="w-100"></div>';
+	// obj = $(html).appendTo('.slides');
+	// obj.css({"left": (i*100)+"%"});
+	$('.slides').append(html);
 	if(i == 0) $(".pager").append('<li class="cir-sel"></li>');
 	else if(i < slides.length-1) $(".pager").append('<li class="cir"></li>');
 }
+$(".slide").each(function(i){
+	$(this).css({"left": (i*100)+"%"});
+});
+/*
+반복문 - Javascript
+for(var i=0; i<10; i++) {
+
+}
+for(var i in Array) {
+
+}
+반복문 - jQuery
+$(객체-여러개).each(function(index){
+
+});
+*/
+
+
+
 
 // 반복, 움직임
 interval = setInterval(function(){

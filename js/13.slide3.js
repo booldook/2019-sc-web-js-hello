5e3197867d8fe3967f890e86079e58536ae229bc
booldook <booldook@gmail.com>
2019-08-05 15:49:22 +0900

    13.slide3

--- js/13.slide3.js
+++ js/13.slide3.js
@@ -14,7 +14,7 @@ var now = 0;														//  맨 처음 .slide의 index
 var end = slides.length - 1;						// 마지막 .slide의 index
 var dir = "R";													// 움직이는 방향 L / R
 var tar; 																// 움직임 목표값 L:-50% / R:0
-var cnt = 9;														// 화면에 보여지는 갯수
+var cnt = 4;														// 화면에 보여지는 갯수
 var slideCnt = cnt + 2;									// li.slide의 갯수
 var slideWid = (100/cnt).toFixed(4); 		// .slide의 width를 소수점 5자리까지..
 var speed = 500;
@@ -22,7 +22,7 @@ var gap = 3000;
 var arr = [];
 var interval;
 
-init();
+init();		// 최초 1번 실행 - 이미지가 들어갈 빈 li를 생성한다.
 function init() {
 	for(var i=0, html=''; i<slideCnt; i++) {
 		html += '<li class="slide p-2" style="flex: '+slideWid+'% 0 0;">';
@@ -31,10 +31,10 @@ function init() {
 	}
 	$(".slides").html(html);
 }
-slideInit();
+
+
+slideInit();	// 애니메이션이 끝난 후 이미지 배치 재조정
 function slideInit() {
-	if(dir == "L") tar = (slideWid * -2) + "%";
-	else tar = 0;
 	// 6개의 칸에 들어갈 index 계산
 	if(now == 0) arr[0] = end;
 	else arr[0] = now - 1;
@@ -47,7 +47,6 @@ function slideInit() {
 		$(".slide").eq(i).find("img").attr("src", slides[arr[i]].src);
 	}
 	$(".slides").css({"left": -slideWid+"%"});
-
 	if(dir == "L") {
 		if(now == end) now = 0;
 		else now++;

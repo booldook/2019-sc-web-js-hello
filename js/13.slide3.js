61aee06ccb6d0038691d676182ec27a579706a17
booldook <booldook@gmail.com>
2019-08-05 14:58:52 +0900

    13.slide3

--- js/13.slide3.js
+++ js/13.slide3.js
@@ -10,12 +10,15 @@ var slides = [
 	{src: "../img/prd8.png"}
 ];
 
-var now = 0;	//  맨 처음 .slide의 index
+var now = 0;									//  맨 처음 .slide의 index
 var end = slides.length - 1;	// 마지막 .slide의 index
-var dir = "L";	// 움직이는 방향 L / R
-var tar; 	// 움직임 목표값 L:-50% / R:0
+var dir = "R";								// 움직이는 방향 L / R
+var tar; 											// 움직임 목표값 L:-50% / R:0
 var speed = 500;
 var gap = 3000;
+var arr = [];
+var interval;
+
 init();
 function init() {
 	for(var i=0, html=''; i<6; i++) {
@@ -25,21 +28,41 @@ function init() {
 	}
 	$(".slides").html(html);
 }
+slideInit();
+function slideInit() {
+	if(dir == "L") tar = "-50%";
+	else tar = 0;
+	// 6개의 칸에 들어갈 index 계산
+	if(now == 0) arr[0] = end;
+	else arr[0] = now - 1;
+	if(now + 3 < end) arr[5] = now + 4;
+	else arr[5] = Math.abs(end - now - 3);
+	for(var i=0; i<4; i++) {
+		if(i + now > 8) arr[(i+1)] = i + now - end - 1;
+		else arr[(i+1)] = now + i;
+	}
+	console.log(arr);
+	for(var i=0; i<6; i++) {
+		$(".slide").eq(i).find("img").attr("src", slides[arr[i]].src);
+	}
+	$(".slides").css({"left": "-25%"});
+
+	if(dir == "L") {
+		if(now == end) now = 0;
+		else now++;
+	}
+	else {
+		if(now == 0) now = end;
+		else now--;
+	}
+}
 
 function slideAni() {
 	if(dir == "L") tar = "-50%";
 	else tar = 0;
-	// 맨처음 li
-	if(now == 0) $(".slide").eq(0).find("img").attr("src", slides[end].src);
-	else $(".slide").eq(0).find("img").attr("src", slides[(now--)].src);
-	// 보여지는 4개의 li
-	for(var i=0; i<4; i++) {
-		if()
-	}
-	$(".slides").stop().animate({"left": tar}, speed, function(){
-
-	});
+	$(".slides").stop().animate({"left": tar}, speed, slideInit);
 }
 
+interval = setInterval(slideAni, gap);
 
 

61d067c9f4df7022d34d32a98a10cd342d39f53b
booldook <booldook@gmail.com>
2019-08-05 15:27:49 +0900

    13.slide3

--- js/13.slide3.js
+++ js/13.slide3.js
@@ -10,10 +10,13 @@ var slides = [
 	{src: "../img/prd8.png"}
 ];
 
-var now = 0;									//  맨 처음 .slide의 index
-var end = slides.length - 1;	// 마지막 .slide의 index
-var dir = "R";								// 움직이는 방향 L / R
-var tar; 											// 움직임 목표값 L:-50% / R:0
+var now = 0;														//  맨 처음 .slide의 index
+var end = slides.length - 1;						// 마지막 .slide의 index
+var dir = "R";													// 움직이는 방향 L / R
+var tar; 																// 움직임 목표값 L:-50% / R:0
+var cnt = 9;														// 화면에 보여지는 갯수
+var slideCnt = cnt + 2;									// li.slide의 갯수
+var slideWid = (100/cnt).toFixed(4); 		// .slide의 width를 소수점 5자리까지..
 var speed = 500;
 var gap = 3000;
 var arr = [];
@@ -21,8 +24,8 @@ var interval;
 
 init();
 function init() {
-	for(var i=0, html=''; i<6; i++) {
-		html += '<li class="slide p-2" style="flex: 25% 0 0;">';
+	for(var i=0, html=''; i<slideCnt; i++) {
+		html += '<li class="slide p-2" style="flex: '+slideWid+'% 0 0;">';
 		html += '<img src="" class="w-100">';
 		html += '</li>';
 	}
@@ -30,22 +33,20 @@ function init() {
 }
 slideInit();
 function slideInit() {
-	if(dir == "L") tar = "-50%";
+	if(dir == "L") tar = (slideWid * -2) + "%";
 	else tar = 0;
 	// 6개의 칸에 들어갈 index 계산
 	if(now == 0) arr[0] = end;
 	else arr[0] = now - 1;
-	if(now + 3 < end) arr[5] = now + 4;
-	else arr[5] = Math.abs(end - now - 3);
-	for(var i=0; i<4; i++) {
-		if(i + now > 8) arr[(i+1)] = i + now - end - 1;
+	for(var i=0; i<=cnt; i++) {
+		if(i + now > end) arr[(i+1)] = i + now - end - 1;
 		else arr[(i+1)] = now + i;
 	}
 	console.log(arr);
-	for(var i=0; i<6; i++) {
+	for(var i=0; i<slideCnt; i++) {
 		$(".slide").eq(i).find("img").attr("src", slides[arr[i]].src);
 	}
-	$(".slides").css({"left": "-25%"});
+	$(".slides").css({"left": -slideWid+"%"});
 
 	if(dir == "L") {
 		if(now == end) now = 0;
@@ -58,7 +59,7 @@ function slideInit() {
 }
 
 function slideAni() {
-	if(dir == "L") tar = "-50%";
+	if(dir == "L") tar = -2*slideWid + "%";
 	else tar = 0;
 	$(".slides").stop().animate({"left": tar}, speed, slideInit);
 }
